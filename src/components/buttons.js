106f775e42fedcb0588a33bba5ef8d1821b169ea
Zonar94 <zonar199421@gmail.com>
2022-01-20 13:37:12 +0100

    updated comments

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -51,6 +51,7 @@ export class buttons extends Component {
     determindedaction = (command) => {
         console.log(command);
         switch (command) {
+            // check if the button has been clicked
             case 'KiOn':
                 if (this.state.isClicked) { 
                          ki += ' button activelight';
@@ -60,10 +61,12 @@ export class buttons extends Component {
                 else if (!this.state.isClicked) {
                    ki -= ' activelight';
                    ki += ' button';
+                   // return the correct button and animation
                     return URL + KiLightOff;    
                 }
                 break;
             case 'MiOn':
+                 // check if the button has been clicked
                 if (this.state.isClicked) {
                     mi += ' button activelight';
                     return URL + MiLight;
@@ -71,12 +74,12 @@ export class buttons extends Component {
                 else if (!this.state.isClicked) {
                      mi -= ' activelight';
                    mi += ' button';
-
-                  
+                      // return the correct button and animation                 
                     return URL + MiLightOff;
                 }
                 break;
             case 'NiOn':
+                 // check if the button has been clicked
                 if (this.state.isClicked) {
                     ni += ' button activelight';
                     return URL + NiLight;
@@ -84,10 +87,12 @@ export class buttons extends Component {
                 else if (!this.state.isClicked) {
                      ni -= ' activelight';
                    ni += ' button';
+                    // return the correct button and animation      
                     return URL + NiLightOff;
                 }
                 break;
                 case 'FanOn':
+                     // check if the button has been clicked
                     if (this.state.isClicked) {
                         fan += ' button activefan';
                         allD += ' button activedevice';
@@ -96,11 +101,12 @@ export class buttons extends Component {
                     else if (!this.state.isClicked) {
                          fan -= ' activefan';
                    fan += ' button';
-                   
+                       // return the correct button and animation   
                         return URL + FanOff;
                     }
                     break;
                 case 'AllLights':
+                     // check if the button has been clicked
                     if (this.state.isClicked) {
                         allL += ' button activelight';
                           ki += ' button activelight';
@@ -117,10 +123,12 @@ export class buttons extends Component {
                    ki += ' button ';
                         mi += ' button ';
                         ni += ' button ';
+                            // return the correct button and animation   
                         return URL + lightsoff;
                     }
                     break;
                 case 'AllDevices':
+                     // check if the button has been clicked
                     if (this.state.isClicked) {
                         ki += ' button activelight';
                         mi += ' button activelight';
@@ -144,6 +152,7 @@ export class buttons extends Component {
                         allL += ' button ';
                         fan += ' button ';
                         return URL + nodev;
+                            // return the correct button and animation   
                     }
                 return URL + MiLight;
             default:
@@ -180,8 +189,9 @@ export class buttons extends Component {
             <div>
                <h1>Studio 154</h1>
                <h3>Guest Access</h3>
-               <div class="container">
-                <button className={ki} onClick={() => this.action("KiOn")}>
+               <div class="container"> 
+               {/* these are the buttons with their svg icons */}
+                <button className={ki} onClick={() => this.action("KiOn")}> 
 <svg xmlns="http://www.w3.org/2000/svg" width="27.9" height="40.583" viewBox="0 0 27.9 40.583">
   <path id="Icon_awesome-lightbulb" data-name="Icon awesome-lightbulb" d="M7.614,36.013a2.537,2.537,0,0,0,.425,1.4l1.355,2.036A2.536,2.536,0,0,0,11.5,40.583H16.4a2.536,2.536,0,0,0,2.112-1.132l1.355-2.036a2.536,2.536,0,0,0,.425-1.4l0-3.04H7.61l0,3.04ZM0,13.95a13.872,13.872,0,0,0,3.453,9.177,22.778,22.778,0,0,1,4.138,7.249c0,.021.006.041.009.062H20.3c0-.021.006-.04.009-.062a22.778,22.778,0,0,1,4.138-7.249A13.947,13.947,0,1,0,0,13.95ZM13.95,7.609A6.348,6.348,0,0,0,7.609,13.95a1.268,1.268,0,0,1-2.536,0A8.887,8.887,0,0,1,13.95,5.073a1.268,1.268,0,0,1,0,2.536Z" transform="translate(0 0)" fill="#000000"/>
 </svg>
